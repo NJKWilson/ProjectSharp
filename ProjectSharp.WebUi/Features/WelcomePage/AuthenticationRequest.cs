9bd28a2c855481f2b47cfd0d3f7aa6609a1c92d1
Nathan Wilson <NJKWilson@outlook.com>
2021-10-07 14:11:34 +0100

    Cleaned out more

--- ProjectSharp.WebUi/Features/WelcomePage/AuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjectSharp.WebUi.Features.WelcomePage
-{
-    public class AuthenticationRequest2
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
