018d067698b400d89f2de5b624744cca8b8afc87
Nathan Wilson <njkwilson@outlook.com>
2021-10-14 21:43:57 +0100

    Made GenerateSalt Static

--- ProjectSharp.Authorisation/Brokers/Password/StringHashBroker.cs
+++ ProjectSharp.Authorisation/Brokers/Password/StringHashBroker.cs
@@ -2,7 +2,7 @@ namespace ProjectSharp.Authorisation.Brokers.Password
 {
     public class StringHashBroker : IStringHashBroker
     {
-        private string GenerateSalt() => BCrypt.Net.BCrypt.GenerateSalt();
+        private static string GenerateSalt() => BCrypt.Net.BCrypt.GenerateSalt();
 
         public string HashString(string inputString)
             => BCrypt.Net.BCrypt.HashPassword(inputString, GenerateSalt());
