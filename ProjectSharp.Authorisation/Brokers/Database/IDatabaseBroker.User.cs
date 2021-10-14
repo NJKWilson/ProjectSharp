7ee99f37ad9c5d4df1d3bf171308c93d6fc3b862
Nathan Wilson <njkwilson@outlook.com>
2021-10-14 18:18:56 +0100

    Removed unused using statement

--- ProjectSharp.Authorisation/Brokers/Database/IDatabaseBroker.User.cs
+++ ProjectSharp.Authorisation/Brokers/Database/IDatabaseBroker.User.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using ProjectSharp.Authorisation.Entities;
 using ProjectSharp.Authorisation.Entities.User;
 
 namespace ProjectSharp.Authorisation.Brokers.Database
